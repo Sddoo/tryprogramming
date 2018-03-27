9a411a424fbde8b53c1efb0b19657321a9472d41
Vladislav <dkflbr-09@mail.ru>
2018-03-27 20:02:24 +0300

    First commit

--- lesson2HARD/script.js
+++ lesson2HARD/script.js
@@ -12,6 +12,11 @@ for (let i=0; i< 7; i++){
 	}
 }
 
+if ("ёжик" < "яблоко")
+	console.log("alarm");
+var a = 0 || "" || 2 || undefined || true || falsе || 3;
+alert(a);
+
 var arr = ['4235','5243','4321','3685','6234','9767','7264'];
 for (let i = 0; i < 7; i++){
   var num = parseInt(+arr[i]/10**(arr[i].length-1));
